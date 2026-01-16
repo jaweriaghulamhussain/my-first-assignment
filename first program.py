29b8400f075521435a738cd33e60bef0b5bf10bb
JAWERIAGHulamHussAin <jaweriaghulamhussain@gmail.com>
2026-01-16 22:29:24 +0500

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,25 @@
+# Area of a Circle
+
+pi = 3.14159
+radius = float(input("Enter the radius of the circle: "))
+
+area = pi * radius * radius
+
+print("Area of the circle is:", area)
+
+# Area of a Square
+
+side = float(input("Enter the side of the square: "))
+
+area = side * side
+
+print("Area of the square is:", area)
+
+# Area of a Triangle
+
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+area = 0.5 * base * height
+
+print("Area of the triangle is:", area)
